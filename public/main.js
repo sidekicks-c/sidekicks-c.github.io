7b35f9930dc7b6d739172e53aa719cbc6bfebaa8
Anne-Victoria <Anne-Victoria@users.noreply.github.com>
2020-12-03 22:39:22 +0100

    Start creating accessibility menu

--- public/main.js
+++ public/main.js
@@ -1,3 +1,12 @@
+const showAccessibilityMenu = () => {
+  const fontSelection = document.getElementById('font-selection')
+  if (fontSelection.classList.contains('display-none')) {
+    fontSelection.classList.remove('display-none');
+  } else {
+    fontSelection.classList.add('display-none');
+  }
+}
+
 const changeFont = (event) => {
   console.log('You selected: ', event.target.value);
   const font = event.target.value;
@@ -12,4 +21,6 @@ const changeFont = (event) => {
   bodyNode.classList.add(`font-${font}`);
 }
 
+document.querySelector('#accessibility-options button')
+  .addEventListener('click',showAccessibilityMenu);
 document.getElementById('font-selection').addEventListener('change', changeFont);
