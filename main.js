c8ebe9386de1e1ca93905549a24aa1c33f46870f
benjamin <bwand@c-base.org>
2020-12-04 23:30:29 +0100

    no-js handling

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
 const showAccessibilityMenu = () => {
-  const fontSelection = document.getElementById('font-selection')
-  if (fontSelection.classList.contains('display-none')) {
-    fontSelection.classList.remove('display-none');
+  const fontSelection = document.getElementById("font-selection");
+  if (fontSelection.classList.contains("display-none")) {
+    fontSelection.classList.remove("display-none");
   } else {
-    fontSelection.classList.add('display-none');
+    fontSelection.classList.add("display-none");
   }
-}
+};
 
-const changeFont = (event) => {
-  console.log('You selected: ', event.target.value);
+const changeFont = event => {
+  console.log("You selected: ", event.target.value);
   const font = event.target.value;
-  const bodyNode = document.getElementsByTagName('body')[0]
+  const bodyNode = document.getElementsByTagName("body")[0];
   bodyNode.classList.remove(
-    'font-SylexiadSans',
-    'font-Lora',
-    'font-AtkinsonHyperlegible',
-    'font-OpenDyslexicThree',
-    'font-Ubuntu',
+    "font-SylexiadSans",
+    "font-Lora",
+    "font-AtkinsonHyperlegible",
+    "font-OpenDyslexicThree",
+    "font-Ubuntu"
   );
   bodyNode.classList.add(`font-${font}`);
-}
+};
 
-document.querySelector('#accessibility-options button')
-  .addEventListener('click',showAccessibilityMenu);
-document.getElementById('font-selection').addEventListener('change', changeFont);
+document
+  .querySelector("#accessibility-options button")
+  .addEventListener("click", showAccessibilityMenu);
+document
+  .getElementById("font-selection")
+  .addEventListener("change", changeFont);
+
+// no-js handling
+for (let el of document.querySelectorAll(".no-js")) el.style.display = "none";
+for (let el of document.querySelectorAll(".js")) el.style.display = "block";
