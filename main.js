8f809f80345934c1fb081a2d6f6494f84cbdcb4b
benjamin <bwand@c-base.org>
2020-12-11 22:07:21 +0100

    h1 bleibt Ubutu (auf Wunsch des Designers))

--- main.js
+++ main.js
@@ -10,15 +10,16 @@ const showAccessibilityMenu = () => {
 const changeFont = event => {
   console.log("You selected: ", event.target.value);
   const font = event.target.value;
-  const bodyNode = document.getElementsByTagName("body")[0];
-  bodyNode.classList.remove(
+  const contentNode = document.getElementsByClassName("content")[0];
+  console.log(contentNode);
+  contentNode.classList.remove(
     "font-SylexiadSans",
     "font-Lora",
     "font-AtkinsonHyperlegible",
     "font-OpenDyslexicThree",
     "font-Ubuntu"
   );
-  bodyNode.classList.add(`font-${font}`);
+  contentNode.classList.add(`font-${font}`);
 };
 
 document
